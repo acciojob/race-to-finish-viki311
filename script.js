77aaca804863e145e9a4f8d14fd634bf4d5d13a1
viki311 <47682424+viki311@users.noreply.github.com>
2023-03-15 21:54:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,16 @@
-let promise1=new Promise((resolve,reject)=>{
-	setTimeout(()=>{
-		resolve("Promise1");
-	},1000)
-})
-let promise2=new Promise((resolve,reject)=>{
-	setTimeout(()=>{
-		resolve("Promise2");
-	},1000)
-})
-let promise3=new Promise((resolve,reject)=>{
-	setTimeout(()=>{
-		resolve("Promise3");
-	},1000)
-})
-let promise4=new Promise((resolve,reject)=>{
-	setTimeout(()=>{
-		resolve("Promise4");
-	},1000)
-})
-let promise5=new Promise((resolve,reject)=>{
-	setTimeout(()=>{
-		resolve("Promise5");
-	},1000)
-})
-
-let ans=window.promises = [promise1,promise2,promise3,promise4,promise5]
-Promise.any().then((res)=>document.getElementByID("output").text.innerHTML=`${res}`;
-// Do not change the code above this
-// add your promises to the array `promises`
+const res = document.getElementById("output");
+ 
+for (let i = 0; i < 5; i++) {
+  const time = Math.floor(Math.random() * 5) + 1;
+  promises.push(
+    new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(time);
+      }, time * 1000);
+    })
+  );
+}
+ 
+Promise.any(promises).then((result) => {
+  res.innerText = result;
+});
